04fee4827f855c95ac5e766901bd88de04f7abb9
overthestream <njhnjh02@naver.com>
2023-06-30 17:51:01 +0900

    feat: initialize DB connection & express app

---
+++ src/config/dataSource.js
@@ -0,0 +1,26 @@
+import typeorm from 'typeorm';
+import dotenv from 'dotenv';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+
+dotenv.config();
+
+const fileURL = import.meta.url;
+const filePath = fileURLToPath(fileURL);
+const dirPath = dirname(filePath);
+
+console.log(String(process.env.DB_PASSWORD.toString()));
+console.log(String(process.env.DB_USER));
+
+const dataSource = new typeorm.DataSource({
+	type: 'postgres',
+	host: process.env.DB_HOST || 'localhost',
+	port: Number(process.env.DB_PORT) || 5432,
+	username: process.env.DB_USER || 'postgres',
+	password: process.env.DB_PASSWORD || 'postgres',
+	database: process.env.DB_NAME || 'gongdol',
+	synchronize: true,
+	entities: [dirPath + '/../entity/*{.js}'],
+});
+
+export default dataSource;
